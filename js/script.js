85e9d555f3dc3b66524427e64a77e598733061c7
Danila Shish <dota2mama2@gmail.com>
2026-02-28 08:59:03 +0300

    28.02_1

--- js/script.js
+++ js/script.js
@@ -89,3 +89,26 @@ closeBtnKafe.addEventListener('click', (event) => {
         window.scrollTo(0, scrollPosition);
         }
 })
+
+// swiper свайпер новостей
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+    el: '.swiper-scrollbar',
+    },
+  });
